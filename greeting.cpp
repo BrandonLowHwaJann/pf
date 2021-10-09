b4390645d3359e7549f7360c86a9edae38fbac80
BrandonLowHwaJann <1211304278@student.mmu.edu.my>
2021-10-09 19:15:35 +0800

    edit greeting

--- greeting.cpp
+++ greeting.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-   cout << "Hello world"<< endl;
-
+   string name;
+   cout << "What is your name: "<< endl;
+   cin >> name;
+   cout << "Hello " << name;
+   
    return 0;
 }
